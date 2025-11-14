921cac0749ac7ea722757ec73090e12e769894e4
Lazzines <eduryagin.07@mail.ru>
2025-11-14 23:21:41 +0300

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -9,12 +9,12 @@ class CircleTestCase(unittest.TestCase):
         self.assertEqual(res, 0)
 
     def test_area_positive(self):
-        res = area(5)
-        self.assertEqual(res, math.pi * 25, places=5)
+    res = circle_area(5)
+    self.assertAlmostEqual(res, math.pi * 25, places=5)
 
     def test_area_negative(self):
-        res = area(-3)
-        self.assertEqual(res, math.pi * 9, places=5)
+    res = circle_area(-3)
+    self.assertAlmostEqual(res, math.pi * 9, places=5)
 
     def test_perimeter_zero(self):
         res = perimeter(0)
